519fee5571045da2c6699f83b1094daa341d21dd
blueberry <blueberryprograms@gmail.com>
2025-07-18 09:20:50 -0400

    moved to golang using fyne

---
+++ storage/storage.go
@@ -0,0 +1,18 @@
+package storage
+
+import (
+	eqb "github.com/regularblueberry/EquiBook/application"
+
+)
+
+
+func InitStorageSystem(app *eqb.App) eqb.AppError{
+	
+	
+
+return eqb.AppError{
+	System:  eqb.SYS_STORAGE, 
+	Code: 	 nil, 
+	Message: "No Error",
+}
+}
